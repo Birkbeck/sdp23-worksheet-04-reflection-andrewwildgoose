9a0ca2ab6bd452499bed91946c8456787a2d0683
andrewwildgoose <a.wildgoose@yahoo.co.uk>
2023-02-09 21:06:08 +0000

    Q1 done

--- src/reflection/DescribeClass.java
+++ src/reflection/DescribeClass.java
@@ -1,10 +1,30 @@
 package reflection;
 
+import java.util.Arrays;
+
 public class DescribeClass {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ClassNotFoundException {
         if (args.length != 1)
             System.out.println("Usage: "); // TODO: describe how to use the utility
 
-        // TODO: implement the functionality for Question 1
+        // TODO: implement the functionality for Question1
+
+        String classNameString = args[0];
+
+        System.out.println("Class info for: " + args[0]);
+        Class<?> classForEmitting = Class.forName(classNameString);
+
+        System.out.println("Interface = " + classForEmitting.isInterface());
+
+        System.out.println(("Declared Fields: " + Arrays.toString(classForEmitting.getDeclaredFields())));
+
+        System.out.println(("Declared Methods: " + Arrays.toString(classForEmitting.getDeclaredMethods())));
+
+        System.out.println(("Declared Constructor: " + Arrays.toString(classForEmitting.getDeclaredConstructors())));
+
+
+
+
+
     }
 }
